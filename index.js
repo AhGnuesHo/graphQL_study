80bb5b0e3c5ef7aa9c7b63d6d7225f6df1788329
seungha <frazzle124@gmail.com>
2023-01-31 18:06:36 +0900

    query 연습

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { ApolloServer, gql } from "apollo-server";
 const typeDefs = gql`
   type Query {
     teams: [Team]
+    team(id: Int): Team
+    equipments: [Equipment]
+    supplies: [Supply]
   }
   type Team {
     id: Int
@@ -12,15 +15,39 @@ const typeDefs = gql`
     mascot: String
     cleaning_duty: String
     project: String
+    supplies: [Supply]
+  }
+  type Equipment {
+    id: String
+    used_by: String
+    count: Int
+    new_or_used: String
+  }
+  type Supply {
+    id: String
+    team: Int
   }
 `;
+
 const resolvers = {
   Query: {
-    teams: () => database.teams,
+    teams: () =>
+      database.teams.map((team) => {
+        team.supplies = database.supplies.filter((item) => {
+          return item.team === team.id;
+        });
+        return team;
+      }),
+    team: (index, id) =>
+      database.teams.find((team) => {
+        return team.id === id.id;
+      }),
+    equipments: () => database.equipments,
+    supplies: () => database.supplies,
   },
 };
 const server = new ApolloServer({ typeDefs, resolvers });
-//typeDefs : gQL에서 사용할 타입구조 지정 
+//typeDefs : gQL에서 사용할 타입구조 지정
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });

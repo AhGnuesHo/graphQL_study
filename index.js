a03258f2a795fdc6d256fb6c1769908671a072db
seungha <frazzle124@gmail.com>
2023-01-31 17:23:38 +0900

    graphQL 서버 생성

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 import { database } from "./database.js";
-console.log(database);
+import { ApolloServer, gql } from "apollo-server";
+const typeDefs = gql`
+  type Query {
+    teams: [Team]
+  }
+  type Team {
+    id: Int
+    manager: String
+    office: String
+    extension_number: String
+    mascot: String
+    cleaning_duty: String
+    project: String
+  }
+`;
+const resolvers = {
+  Query: {
+    teams: () => database.teams,
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+//typeDefs : gQL에서 사용할 타입구조 지정 
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
